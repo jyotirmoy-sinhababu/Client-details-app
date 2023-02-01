ecbc9311672092cc42637da83d2a040f0a12892e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-01 15:33:47 +0530

    detail comp created and data is fetched using axios

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Nav from '../../components/nav/Nav';
+import Detail from '../../components/details/Detail';
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <Detail />
     </>
   );
 };
