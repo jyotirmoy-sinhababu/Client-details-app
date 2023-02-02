202b0a0d99f1e970dbf155a40819bb8bef8feda7
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-02 10:24:41 +0530

    re-arrangement made

--- src/components/details/Detail.js
+++ src/components/details/Detail.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
-import axios from 'axios';
-
-import { useState, useEffect } from 'react';
-
-const Detail = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    try {
-      axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
-        console.log(res.data);
-        setData(res.data);
-      });
-    } catch (err) {}
-  }, []);
-
-  return <div></div>;
+const Detail = ({ data }) => {
+  return <></>;
 };
 
 export default Detail;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,11 +3,38 @@ import React from 'react';
 import Nav from '../../components/nav/Nav';
 import Detail from '../../components/details/Detail';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      });
+    } catch (err) {}
+  }, []);
+
   return (
     <>
       <Nav />
-      <Detail />
+
+      {data ? (
+        data.map((item) => {
+          return (
+            <div key={item.id}>
+              <Detail data={item} />
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <p>Loading ...</p>
+        </div>
+      )}
     </>
   );
 };
