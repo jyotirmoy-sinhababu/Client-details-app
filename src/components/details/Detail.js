e68678933f3414d5efabb60b06b5ec25c3584744
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-02 11:24:10 +0530

    data is arranged

--- src/components/details/Detail.js
+++ src/components/details/Detail.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import { useState } from 'react';
+
 const Detail = ({ data }) => {
-  return <></>;
+  const [viewDetails, setViewDetails] = useState(false);
+
+  const showDetails = () => {
+    setViewDetails(true);
+  };
+
+  console.log(viewDetails);
+
+  return (
+    <>
+      <div className=''>
+        {' '}
+        <div>
+          <p>{data.username}</p>
+        </div>
+        <div>
+          <p>CONTACT</p>
+          <p>{data.name}</p>
+        </div>
+        <div>
+          <p>CITY</p>
+          <p>{data.address.city}</p>
+        </div>
+        <div>
+          <p>STATE</p>
+          <p>{data.address.street}</p>
+        </div>
+        <div>
+          <button onClick={showDetails}>View Details</button>
+        </div>
+      </div>
+      {viewDetails ? (
+        <div>
+          <p>hi</p>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default Detail;
